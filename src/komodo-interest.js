68a8cb418a1b1067a8074a59927706cce1fc104e
pbca26 <pbca26@gmail.com>
2018-02-27 13:32:09 +0300

    komodo interest utils

---
+++ src/komodo-interest.js
@@ -0,0 +1,27 @@
+const kmdCalcInterest = (locktime, value) => { // value in sats
+  const timestampDiff = Math.floor(Date.now() / 1000) - locktime - 777;
+  const hoursPassed = Math.floor(timestampDiff / 3600);
+  const minutesPassed = Math.floor((timestampDiff - (hoursPassed * 3600)) / 60);
+  const secondsPassed = timestampDiff - (hoursPassed * 3600) - (minutesPassed * 60);
+  let timestampDiffMinutes = timestampDiff / 60;
+  let interest = 0;
+
+  // calc interest
+  if (timestampDiffMinutes >= 60) {
+    if (timestampDiffMinutes > 365 * 24 * 60) {
+      timestampDiffMinutes = 365 * 24 * 60;
+    }
+    timestampDiffMinutes -= 59;
+
+    // TODO: check if interest is > 5% yr
+    // calc ytd and 5% for 1 yr
+    // const hoursInOneYear = 365 * 24;
+    // const hoursDiff = hoursInOneYear - hoursPassed;
+
+    interest = Math.floor(((Number(value) * 0.00000001) / 10512000) * timestampDiffMinutes);
+  }
+
+  return interest;
+}
+
+module.exports = kmdCalcInterest;
