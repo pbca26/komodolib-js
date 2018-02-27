7404548a48a641e6bc6f7a29e12729081e010047
pbca26 <pbca26@gmail.com>
2018-02-27 13:32:36 +0300

    key pair / seed utils

---
+++ src/coin-helpers.js
@@ -0,0 +1,88 @@
+const _komodoCoins = [
+  'SUPERNET',
+  'REVS',
+  'PANGEA',
+  'DEX',
+  'JUMBLR',
+  'BET',
+  'CRYPTO',
+  'COQUI',
+  'HODL',
+  'MSHARK',
+  'BOTS',
+  'MGW',
+  'MVP',
+  'KV',
+  'CEAL',
+  'MESH',
+  'WLC',
+  'MNZ',
+  'CHIPS',
+  'KMD',
+  'KOMODO',
+  'AXO',
+  'ETOMIC',
+  'BTCH',
+  'VOTE',
+  'BEER',
+  'PIZZA'
+];
+
+const isKomodoCoin = (coin) => {
+  return (_komodoCoins.find((element) => {
+    return element === coin.toUpperCase();
+  }));
+}
+
+// TODO: add at least 2 explorers per coin
+const explorerList = {
+  KMD: 'http://www.kmdexplorer.ru',
+  MSHARK: 'http://MSHARK.explorer.supernet.org',
+  REVS: 'http://revs.explorer.supernet.org',
+  SUPERNET: 'http://SUPERNET.explorer.supernet.org',
+  DEX: 'http://DEX.explorer.supernet.org',
+  PANGEA: 'http://PANGEA.explorer.supernet.org',
+  JUMBLR: 'http://JUMBLR.explorer.supernet.org',
+  BET: 'http://BET.explorer.supernet.org',
+  CRYPTO: 'http://CRYPTO.explorer.supernet.org',
+  HODL: 'http://HODL.explorer.supernet.org',
+  SHARK: 'http://SHARK.explorer.supernet.org',
+  BOTS: 'http://BOTS.explorer.supernet.org',
+  MGW: 'http://MGW.explorer.supernet.org',
+  WLC: 'http://WIRELESS.explorer.supernet.org',
+  CHIPS: 'http://CHIPS1.explorer.supernet.org',
+  COQUI: 'https://explorer.coqui.cash',
+  MNZ: 'https://www.mnzexplorer.com',
+  BTCH: 'http://www.btch.host',
+  BTC: 'https://blockchain.info',
+  HUSH: 'https://explorer.myhush.org',
+  PIZZA: 'http://pizza.komodochainz.info',
+  BEER: 'http://beer.komodochainz.info',
+  QTUM: 'https://explorer.qtum.org',
+  LTC: 'https://live.blockcypher.com/ltc',
+  DOGE: 'https://live.blockcypher.com/doge',
+  DASH: 'https://live.blockcypher.com/dash',
+  MONA: 'https://bchain.info/MONA',
+  VIA: 'https://explorer.viacoin.org',
+  VTC: 'http://explorer.vertcoin.info',
+  NMC: 'https://namecha.in',
+  DGB: 'https://digiexplorer.info',
+  CRW: 'http://ex.crownlab.eu',
+  BTG: 'https://btgexplorer.com',
+  BCH: 'https://bitcoincash.blockexplorer.com',
+  ZCL: 'http://explorer.zclmine.pro',
+  ZMY: 'https://myriadexplorer.com',
+  BTX: 'http://explorer.bitcore.cc',
+  BTCZ: 'https://explorer.bitcoinz.site',
+  HODLC: 'http://www.fuzzbawls.pw/explore/HOdlcoin/tx.php?tx=',
+  SIB: 'https://chain.sibcoin.net/en/tx/',
+  ZEC: 'https://explorer.zcha.in/transactions/',
+  BLK: 'https://explorer.coinpayments.net/transaction.php?chain=4&hash=',
+  ARG: 'https://prohashing.com/explorer/Argentum/',
+  FAIR: 'https://chain.fair.to/transaction?transaction=',
+};
+
+module.exports = {
+  isKomodoCoin,
+  explorerList,
+};
---
+++ src/keys.js
@@ -0,0 +1,139 @@
+const sha256 = require('js-sha256');
+const crypto = require('crypto');
+const bigi = require('bigi');
+const bitcoinZcash = require('bitcoinjs-lib-zcash');
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinPos = require('bitcoinjs-lib-pos');
+const bs58check = require('bs58check');
+
+const addressVersionCheck = (network, address) => {
+  try {
+    const _b58check = shepherd.isZcash(network.toLowerCase()) ? bitcoinZcash.address.fromBase58Check(address) : bitcoin.address.fromBase58Check(address);
+
+    if (_b58check.version === network.pubKeyHash) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch(e) {
+    return 'Invalid pub address';
+  }
+}
+
+const wifToWif = (wif, network) => {
+  let key;
+
+  if (network.isZcash) {
+    key = new bitcoinZcash.ECPair.fromWIF(wif, network, true);
+  } else {
+    key = new bitcoin.ECPair.fromWIF(wif, network, true);
+  }
+
+  return {
+    pub: key.getAddress(),
+    priv: key.toWIF(),
+  };
+}
+
+const seedToWif = (seed, network, iguana) => {
+  const hash = sha256.create().update(seed);
+  let bytes = hash.array();
+
+  if (iguana) {
+    bytes[0] &= 248;
+    bytes[31] &= 127;
+    bytes[31] |= 64;
+  }
+
+  const d = bigi.fromBuffer(bytes);
+  let keyPair;
+
+  if (network.isZcash) {
+    keyPair = new bitcoinZcash.ECPair(d, null, { network });
+  } else {
+    keyPair = new bitcoin.ECPair(d, null, { network });
+  }
+
+  const keys = {
+    pub: keyPair.getAddress(),
+    priv: keyPair.toWIF(),
+  };
+
+  return keys;
+}
+
+// login like function
+const stringToWif = (string, network, iguana) => {
+  let _wifError = false;
+  let isWif = false;
+  let keys;
+
+  // watchonly
+  if (seed.match('^[a-zA-Z0-9]{34}$')) {
+    return {
+      priv: string,
+      pub: string,
+    };
+  } else {
+    try {
+      bs58check.decode(string);
+      isWif = true;
+    } catch (e) {}
+
+    if (isWif) {
+      try {
+        if (network.isZcash) {
+          key = new bitcoinZcash.ECPair.fromWIF(wif, network, true);
+        } else {
+          key = new bitcoin.ECPair.fromWIF(wif, network, true);
+        }
+
+        keys = {
+          priv: key.toWIF(),
+          pub: key.getAddress(),
+        };
+      } catch (e) {
+        _wifError = true;
+      }
+    } else {
+      keys = seedToWif(string, network, iguana);
+    }
+  }
+
+  return _wifError ? 'error' : keys;
+}
+
+const bip39Search = (seed, network, matchPattern, addressDepth, accountsCount, includeChangeAddresses) => {
+  const seed = bip39.mnemonicToSeed(seed);
+  const hdMaster = bitcoin.HDNode.fromSeedBuffer(seed, network);
+  const matchPattern = match;
+  const _defaultAddressDepth = addressDepth;
+  const _defaultAccountCount = accountsCount;
+  let _addresses = [];
+  let _matchingKey;
+
+  for (let i = 0; i < _defaultAccountCount; i++) {
+    for (let j = 0; j < includeChangeAddresses ? 2 : 1; j++) {
+      for (let k = 0; k < _defaultAddressDepth; k++) {
+        const _key = hdMaster.derivePath(`m/44'/141'/${i}'/${j}/${k}`);
+
+        if (_key.keyPair.getAddress() === matchPattern) {
+          _matchingKey = {
+            pub: _key.keyPair.getAddress(),
+            priv: _key.keyPair.toWIF(),
+          };
+        }
+      }
+    }
+  }
+
+  return _matchingKey ? _matchingKey : 'address is not found';
+});
+
+module.exports = {
+  bip39Search,
+  addressVersionCheck,
+  wifToWif,
+  seedToWif,
+  stringToWif,
+};
