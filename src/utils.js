db81c6370364ba8a07a0a64b5844bd4a925b04d9
pbca26 <pbca26@gmail.com>
2018-08-28 19:33:56 +0300

    utils display rounding fix

--- src/utils.js
+++ src/utils.js
@@ -77,28 +77,10 @@ const isPositiveNumber = (value) => {
 }
 
 // display rounding
-const formatValue = (formatValue) => {
-  const _valueToStr = formatValue.toString();
-
-  if (_valueToStr.indexOf('.') === -1) {
-    return formatValue;
-  } else {
-    if (_valueToStr) {
-      const _decimal = _valueToStr.substr(_valueToStr.indexOf('.') + 1, _valueToStr.length);
-      let newVal = _valueToStr.substr(0, _valueToStr.indexOf('.') + 1);
-
-      for (let i = 0; i < _decimal.length; i++) {
-        if (_decimal[i] === '0') {
-          newVal = newVal + _decimal[i];
-        } else {
-          newVal = newVal + _decimal[i];
-          break;
-        }
-      }
-
-      return newVal;
-    }
-  }
+// ref: https://stackoverflow.com/questions/3612744/remove-insignificant-trailing-zeros-from-a-number
+const formatValue = (value) => {
+  const c = Math.pow(10, 8);
+  return Math.trunc(value * c) / c;
 }
 
 const formatBytes = (bytes, decimals) => {
