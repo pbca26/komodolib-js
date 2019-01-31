5fa4fe2ca566302bf4350393e8312a20a31848a9
pbca26 <pbca26@gmail.com>
2019-01-31 17:19:50 +0300

    message sign/verify eth

--- build/message.js
+++ build/message.js
@@ -2,15 +2,16 @@
 
 var bitcoinMessage = require('bitcoinjs-message');
 var wif = require('wif');
+var ethers = require('ethers');
 
-var sign = function sign(_wif, message) {
+var signBTC = function signBTC(_wif, message) {
   var keyPair = wif.decode(_wif);
 
   var signature = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed);
   return signature.toString('base64');
 };
 
-var verify = function verify(address, message, sig) {
+var verifyBTC = function verifyBTC(address, message, sig) {
   try {
     return bitcoinMessage.verify(message, address, sig);
   } catch (e) {
@@ -18,7 +19,38 @@ var verify = function verify(address, message, sig) {
   };
 };
 
+var signETH = function signETH(privKey, message) {
+  var signingKey = new ethers.utils.SigningKey(privKey);
+  var messageBytes = ethers.utils.toUtf8Bytes(message);
+  var messageDigest = ethers.utils.keccak256(messageBytes);
+  var signature = signingKey.signDigest(messageDigest);
+
+  return Buffer.from(JSON.stringify(signature)).toString('base64');
+};
+
+var verifyETH = function verifyETH(address, message, sig) {
+  var signature = void 0;
+
+  try {
+    signature = JSON.parse(new Buffer(sig, 'base64').toString());
+  } catch (e) {
+    return false;
+  }
+
+  var messageBytes = ethers.utils.toUtf8Bytes(message);
+  var messageDigest = ethers.utils.keccak256(messageBytes);
+  var recoveredAddress = ethers.utils.recoverAddress(messageDigest, signature);
+
+  return address === recoveredAddress ? true : false;
+};
+
 module.exports = {
-  sign: sign,
-  verify: verify
+  btc: {
+    sign: signBTC,
+    verify: verifyBTC
+  },
+  eth: {
+    sign: signETH,
+    verify: verifyETH
+  }
 };
--- src/message.js
+++ src/message.js
@@ -1,14 +1,15 @@
 const bitcoinMessage = require('bitcoinjs-message');
 const wif = require('wif');
+const ethers = require('ethers');
 
-const sign = (_wif, message) => {
+const signBTC = (_wif, message) => {
   const keyPair = wif.decode(_wif);
   
   const signature = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed);
   return signature.toString('base64');
 };
 
-const verify = (address, message, sig) => {
+const verifyBTC = (address, message, sig) => {
   try {
     return bitcoinMessage.verify(message, address, sig);
   } catch (e) {
@@ -16,7 +17,38 @@ const verify = (address, message, sig) => {
   };
 };
 
+const signETH = (privKey, message) => {
+  const signingKey = new ethers.utils.SigningKey(privKey);
+  const messageBytes = ethers.utils.toUtf8Bytes(message);
+  const messageDigest = ethers.utils.keccak256(messageBytes);
+  const signature = signingKey.signDigest(messageDigest);
+  
+  return Buffer.from(JSON.stringify(signature)).toString('base64');
+};
+
+const verifyETH = (address, message, sig) => {
+  let signature;
+  
+  try {
+    signature = JSON.parse(new Buffer(sig, 'base64').toString());
+  } catch (e) {
+    return false;
+  }
+
+  const messageBytes = ethers.utils.toUtf8Bytes(message);
+  const messageDigest = ethers.utils.keccak256(messageBytes);
+  const recoveredAddress = ethers.utils.recoverAddress(messageDigest, signature);
+  
+  return address === recoveredAddress ? true : false;
+};
+
 module.exports = {
-  sign,
-  verify,
+  btc: {
+    sign: signBTC,
+    verify: verifyBTC,
+  },
+  eth: {
+    sign: signETH,
+    verify: verifyETH,
+  },
 };
