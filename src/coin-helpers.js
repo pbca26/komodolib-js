4388e3efe567cda9b4cc64220cd4e7312eb39c37
Nikhil M <niksmac@users.noreply.github.com>
2019-02-06 15:33:29 +0530

    Add KOIN block explorer

--- src/coin-helpers.js
+++ src/coin-helpers.js
@@ -66,6 +66,7 @@ const explorerList = {
   KMD: 'https://www.kmdexplorer.io',
   // KMD asset chains
   DION: 'https://explorer.dionpay.com',
+  KOIN: 'http://live.block.koinon.cloud',
   PTX: 'http://explorer1.patenttx.com',
   ZILLA: 'http://zilla.explorer.dexstats.info',
   MGNX: 'http://mgnx.explorer.dexstats.info',
