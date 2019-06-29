d08cf8272ef3f950cd39e62d7b12f69dd0483c34
Emmanuel <emmanuel.utrum@pm.me>
2019-06-29 20:31:52 +0000

    add DP

--- src/electrum-servers.js
+++ src/electrum-servers.js
@@ -539,6 +539,10 @@ let _electrumServers = {
     'electrum2.cipig.net:10019:tcp',
     'electrum3.cipig.net:10019:tcp',
   ],
+  dp: [
+    'dp1.electrum.komodo.top:20011',
+    'dp2.electrum.komodo.top:20012',
+  ],
 };
 _electrumServers.zexo = _electrumServers.zex;
 
