a10b0a7af2a5284c9b4860963d6eadec931bb544
pbca26 <pbca26@gmail.com>
2019-08-14 19:01:17 +0300

    block add parseBlockToJSON function

--- src/block.js
+++ src/block.js
@@ -7,7 +7,26 @@ const parseBlock = (hex, network) => {
   return block;
 };
 
-const electrumMerkleRoot = (parsedBlock) => {
+const parseBlockToJSON = (hex, network) => {
+  if (typeof hex === 'string') {            
+    hex = parseBlock(hex, network);
+
+    if (hex.hasOwnProperty('merkle_root')) {
+      hex.merkleRoot = hex.merkle_root;
+      delete hex.merkle_root;
+    }
+
+    for (let key in hex) {
+      if (typeof hex[key] === 'object') {
+        hex[key] = key !== 'solution' ? hex[key].reverse().toString('hex') : hex[key].toString('hex');
+      }
+    }
+  }
+
+  return hex;
+};
+
+const electrumMerkleRoot = (parsedBlock) => { // legacy
   if (parsedBlock.merkleRoot) { // electrum protocol v1.4
     return new Buffer(parsedBlock.merkleRoot.reverse()).toString('hex');
   } else {
@@ -17,5 +36,6 @@ const electrumMerkleRoot = (parsedBlock) => {
 
 module.exports = {
   parseBlock,
+  parseBlockToJSON,
   electrumMerkleRoot,
 };
