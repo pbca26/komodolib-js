e7f936a50c6cf59b1b35f25d882d37644eb2626a
pbca26 <pbca26@gmail.com>
2021-01-06 18:21:51 +0300

    add ledger transport imports file

---
+++ src/hw/kmd/ledger-transport.js
@@ -0,0 +1,7 @@
+import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
+import TransportWebBLE from '@ledgerhq/hw-transport-web-ble';
+
+module.exports = {
+  webusb: TransportWebUSB,
+  ble: TransportWebBLE,
+};
